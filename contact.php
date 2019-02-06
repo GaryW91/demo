3d950c2bedf5a85c837f34ca769823122eeaba09
GaryW91 <gw@g-williamson.co.uk>
2019-02-06 17:18:01 +0000

    forgot the email field

--- contact.php
+++ contact.php
@@ -14,8 +14,8 @@ $fromEmail = 'noreply@email.com';
 $fromName = 'No Reply Email';
 
 // an email address that will receive the email with the output of the form
-$sendToEmail = 'name@mydomain.com';
-$sendToName = 'New Website Email Message';
+$sendToEmail = 'gazza_aye@hotmail.com';
+$sendToName = 'Gary';
 
 // subject of the email
 $subject = 'New message from contact form';
